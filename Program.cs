53640e6e99c5ddae9b39771a2cd72779bde3c71e
RajatSharma2004 <rajatsharmayo4@gmail.com>
2025-06-17 00:05:44 +0530

    Initial commit - C# console app

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MyProgram
+{
+  class MainClass
+  {
+    public static void Main(string[] args)
+    {
+      Console.WriteLine("Hello ! ");
+      Console.Write("I am Rajat ");
+      Console.WriteLine("Bye");
+
+      fact();
+
+
+
+    }
+    public static void fact()
+    {
+      Console.Write("Enter a number ");
+      int number = Convert.ToInt32(Console.ReadLine());
+
+      int factorial = 1;
+      for (int i = 1; i <= number; i++)
+      {
+        factorial *= i;
+      }
+      Console.WriteLine("The factorial of number is : " + factorial);
+
+    }
+    
+  }
+}
